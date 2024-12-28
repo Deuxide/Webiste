9f7dcad4531d96e990c425a910bec8d92831883b
Deuxide <ilmaygpu@gmail.com>
2024-12-28 10:46:02 +0700

    Initial commit with project structure and assets

---
+++ index.js
@@ -0,0 +1,73 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDh-yS0dsCY8ByYLFPiOU9h11m8kV5JFzk",
+    authDomain: "deuxite.firebaseapp.com",
+    databaseURL: "https://deuxite-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "deuxite",
+    storageBucket: "deuxite.firebasestorage.app",
+    messagingSenderId: "477722605776",
+    appId: "1:477722605776:web:a1e077450c894b4653c6d5",
+    measurementId: "G-5G6484FCCL"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+import {getDatabase, ref, child, get, set, remove} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
+
+const db = getDatabase();
+
+let FnameInp = document.getElementById('FnameInp');
+let AnswerInp = document.getElementById('AnswerInp')
+
+let addbtn = document.getElementById('addbtn')
+let retbtn = document.getElementById('retbtn')
+let delbtn = document.getElementById('delbtn')
+
+function adddata(){
+    set(ref(db, 'Answer/' + FnameInp.value), {
+    answer: AnswerInp.value
+    })
+    .then(()=>{
+    alert("data added succesfully");
+    })
+    .catch(()=>{
+    alert("unsuccsesful");
+    console.log(error);
+    })
+}
+
+function retdata(){
+    const dbRef = ref(db);
+
+    get(child(dbRef, 'Answer/' + FnameInp.value)).then((snapshot)=>{
+    if(snapshot.exists()){
+        AnswerInp.value = snapshot.val().answer;
+    }
+
+    else {
+        alert("You haven't answered bruh -_-")
+    }
+    })
+    .catch(()=>{
+    alert("unsuccsesful");
+    console.log(error);
+    })
+}
+
+function deletedata(){
+    remove(ref(db, 'Answer/' + FnameInp.value))
+    .then(()=>{
+    alert("data deleted succesfully");
+    })
+    .catch(()=>{
+    alert("unsuccsesful");
+    console.log(error);
+    })
+}
+
+addbtn.addEventListener('click', adddata);
+retbtn.addEventListener('click', retdata);
+delbtn.addEventListener('click', deletedata);
